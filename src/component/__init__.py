2a46b28e9c90f3aa24013900bbd99bb62006d054
Niyojit Choudhari <niyojitchoudhari@gmail.com>
2025-06-28 10:17:52 +0530

    added frontend home page

---
+++ src/component/__init__.py
@@ -0,0 +1,6 @@
+from component.explore import show_home
+from component.holding import show_install
+from component.sip_calculator import show_user_guide
+from component.stock_transaction import show_api
+from component.contact_us import show_examples
+from component.community import show_community
